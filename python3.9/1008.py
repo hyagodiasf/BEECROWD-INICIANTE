ee877b18a8cb17cfb231f3fe06b919fa73efb744
Hyago Diaz <hyagodiasf@gmail.com>
2022-04-24 20:29:33 -0300

    Salário

---
+++ python3.9/1008.py
@@ -0,0 +1,6 @@
+NUMERO = int(input())
+HORAS_T = int(input())
+SALARIO = float(input())
+SAL_FUN = HORAS_T * SALARIO
+print("NUMBER = {}".format(NUMERO))
+print("SALARY = U$ {:.2f}".format(SAL_FUN))
