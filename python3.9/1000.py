1a78de429fc63ed80ed287e7f279d13b76a4c560
Hyago Diaz <hyagodiasf@gmail.com>
2022-04-24 16:56:19 -0300

    codificação 1000

--- python3.9/1000.py
+++ python3.9/1000.py
@@ -1,10 +1,1 @@
-""""""""""""""""""
-"""""PYTHON 3.9"""
-""""""""""""""""""
-
-"RESOLUÇÃO DO PROBLEMA 1000 'HELLO WORLD!'"
-
-    
-
 print("Hello world!")
-
---
+++ python3.9/1002.py
@@ -0,0 +1,5 @@
+raio = float(input())
+n = 3.14159
+area = n * pow(raio,2)
+
+print("A={:.4f}".format(area))
